10af0c47471767b52498ca4736b074ac67076dc9
Roger_9822 <mulgaonkarsumant163@gmail.com>
2025-12-24 00:55:25 +0530

    Initial commit

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -4,9 +4,10 @@ import { OrdersService } from './orders.service';
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
-@Post()
+@Post()  
 createOrder(@Body() body) {
   return this.ordersService.createOrder(body.userId, body.productId);
 }
+
 
 }
